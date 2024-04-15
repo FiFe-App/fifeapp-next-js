6d9ecd07570eb68b0219772c2c4aaeb119d4f277
Abel Erdesz <abel.erdesz@accretivetg.com>
2024-04-15 21:21:14 +0200

    simplify base css files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,23 @@
-
-import type { Metadata } from 'next'
-import { Space_Mono } from 'next/font/google'
-import './globals.css'
-import '@radix-ui/themes/styles.css';
-import BasePage from '@/components/BasePage'
-import Error from 'next/error'
-import { persistor, store } from '../lib/store'
-import FirebaseProvider, { FirebaseContext } from '../firebase/firebase'
-import { Theme, ThemePanel } from '@radix-ui/themes'
+import FirebaseProvider from '../firebase/firebase'
+import { Theme } from '@radix-ui/themes'
 import MyProvider from '@/lib/myProvider'
-import { ErrorBoundary } from 'next/dist/client/components/error-boundary'
-import { useWindowSize } from '@/lib/functions';
-
-const space_mono = Space_Mono({ subsets: ['latin'], weight: '400' })
+import '@/css/layout.css'
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
-
-
     return (
         <MyProvider>
             <FirebaseProvider>
-                  <html lang="en">
-                      <body className={space_mono.className}>
-                        <Theme scaling='90%' accentColor='amber'>
-                          {children}
+                <html lang="en">
+                    <body>
+                        <Theme scaling="90%" accentColor="amber">
+                            {children}
                         </Theme>
-                        </body>
-                  </html>
+                    </body>
+                </html>
             </FirebaseProvider>
         </MyProvider>
     )
