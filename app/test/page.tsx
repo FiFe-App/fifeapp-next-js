3bbd32d7a988aa2502401c6c063ecb28e85a45fd
Akos <kristofakos1229@gmail.com>
2024-04-21 18:16:15 +0200

    lint fix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,21 +1,9 @@
 import { buziness } from "@prisma/client";
-import { Box, Heading, Text } from "@radix-ui/themes";
-import axios from "axios";
-import { useEffect } from "react";
+import { Box, Text } from "@radix-ui/themes";
 
-async function getData() {
-    const res = await fetch('http://localhost:3000/api',{method:'GET'})
-    
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    const d = res.json();
-    
-    return d
-  }
 
 export default async function Page() {
-    const data:Array<buziness> = (await getData()).results
+    const data:Array<buziness> = []//(await getData()).results
     
     return (<Box>
             {!!data.length && data?.map((e,i)=>{
